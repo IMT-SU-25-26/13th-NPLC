78face9fbdf1c29f39797cd6af3aee9e6fcb6e5e
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-09-03 16:49:23 +0700

    business plan templete responsive structure page.tsx

--- src/app/(competitions)/register/business-plan/page.tsx
+++ src/app/(competitions)/register/business-plan/page.tsx
@@ -21,12 +21,15 @@ export default async function Page() {
     session.user.id,
     competitionId
   );
-  
 
-  const alreadyRegisteredForCompetitiveProgramming = await checkCompetitionPageAccess(session.user.id, "cmegpb4cl0000hke9j8b2vg3f");
-  
-  if(alreadyRegisteredForCompetitiveProgramming){
-    redirect("/register/already-registered-at-competitive-programming")
+  const alreadyRegisteredForCompetitiveProgramming =
+    await checkCompetitionPageAccess(
+      session.user.id,
+      "cmegpb4cl0000hke9j8b2vg3f"
+    );
+
+  if (alreadyRegisteredForCompetitiveProgramming) {
+    redirect("/register/already-registered-at-competitive-programming");
   }
 
   if (alreadyRegisteredForBusinessPlan) {
@@ -35,50 +38,32 @@ export default async function Page() {
 
   return (
     <div className="overflow-hidden">
-      <div className="cp-regis-background-img-container w-full overflow-hidden bg-gradient-to-b from-[#111114] to-[#090A1E] relative">
+      <div className="cp-regis-main-wrapper-container relative flex lg:gap-8 flex-col justify-start items-center w-full min-h-screen">
+        <div className="z-[5] absolute w-full h-full bg-gradient-to-b from-[0%] from-[#2a0335]/50 via-[43%] via-[#6258D1]/50 to-[100%] to-[#00CBC1]/50 blue-light-top"></div>
+        <div className="home-purple-light-middle absolute bottom-[-5rem] bg-[#97156A] w-[300px] h-[500px] md:w-[600px] md:h-[600px] xl:w-[1100px] xl:h-[900px] rounded-full blur-[100px] z-[0]"></div>
         <Image
-          className="cp-regis-gradient-light-bg absolute z-[1] w-full bottom-[40%] sm:bottom-[40%] lg:bottom-[-30%] h-[35rem] sm:h-auto"
-          src={"/backgrounds/GradientLightBG.webp"}
-          width={1000}
-          height={1000}
-          alt="background-gradient"
-        />
-        <Image
-          className="cp-regis-light-middle  absolute z-[2] w-full bottom-[40%] sm:bottom-[35%] lg:bottom-[20%] h-auto"
-          src={"/backgrounds/LightTengah.webp"}
-          width={1000}
-          height={1000}
-          alt="light-tengah"
-        />
-        <Image
-          className="cp-regis-blue-building absolute z-[3] bottom-[40%] sm:bottom-[35%] lg:bottom-[-4%] w-screen h-auto"
           src={"/backgrounds/BangunanBelakangBiru.svg"}
+          alt="Background"
           width={100}
+          draggable={false}
           height={100}
-          alt="bagunan-biru"
+          className="z-[1] restriction-blue-building will-change-transform absolute w-screen h-auto bottom-[0rem]"
         />
         <Image
-          className="cp-regis-purple-building absolute z-[4] bottom-[35%] sm:bottom-[30%] lg:bottom-[-4%] w-screen h-auto"
           src={"/backgrounds/BangunanDepanUngu.svg"}
+          alt="Background"
           width={100}
+          draggable={false}
           height={100}
-          alt="bangunan-ungu"
+          className="z-[2] restriction-purple-building absolute will-change-transform w-screen h-auto bottom-[-5rem]"
         />
-        <Image
-          className="cp-regis-front-light-gradient absolute z-[5] bottom-[30%] sm:bottom-[25%] lg:bottom-0 w-screen h-auto"
-          src={"/backgrounds/FrontGradientLightBG.svg"}
-          width={100}
-          height={100}
-          alt="front-light"
-        />
-
-        <div className=" relative z-20 flex gap-4 flex-col items-center justify-center min-h-screen w-dvw">
+        <div className="relative z-10 flex flex-col gap-12 items-center justify-center w-full h-full">
           <Image
-            className=" z-[10] w-[60%] sm:w-1/3 mt-0 sm:mt-15 md:mt-23 lg:mt-25 xl:mt-30 h-auto"
+            className="cp-competition-text z-[10] w-[60%] sm:w-1/3 h-auto"
             src={"/register/competition-text.webp"}
             width={1000}
             height={1000}
-            alt="Competition Text"
+            alt="background-gradient"
           />
           <BusinessPlanRegistrationForm
             competitionTitle="Business Plan"
@@ -86,10 +71,8 @@ export default async function Page() {
             userId={session.user.id}
           />
         </div>
-
-        <div className="under-stair bg-[#090A1E] absolute w-dvw h-[20rem] bottom-[-10%] z-[9]"></div>
         <Image
-          className="cp-regis-stairs absolute z-[10] w-full bottom-[28%] sm:bottom-[20%] lg:bottom-[-8%] h-auto"
+          className="z-[10] absolute w-full bottom-[-1%] h-auto"
           src={"/backgrounds/Stairs.svg"}
           width={1000}
           height={1000}
