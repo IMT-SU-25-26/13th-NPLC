efa32d978fcbdd8e54e50a5aef675d231331f676
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-09-03 14:01:10 +0700

    register templete account

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -5,7 +5,6 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import "@/styles/registeraccount-bg.css";
-import MobileWaves from "@/components/MobileWaves";
 
 export default async function Page() {
   const session = await auth.api.getSession({
@@ -16,45 +15,33 @@ export default async function Page() {
     redirect("/already-logged-in");
   }
   return (
-    <div className="rg-regis-main-wrapper-container overflow-hidden min-h-screen">
-      <MobileWaves className="top-0 w-screen h-auto z-[3]"/>
-      <div className="rg-regis-background-img-container w-full bg-gradient-to-b from-[#111114] to-[#090A1E] relative">
+    <div className="overflow-hidden">
+      <div className="rg-regis-main-wrapper-container relative min-h-screen w-screen overflow-hidden bg-gradient-to-b from-[#111114] to-[#090A1E]">
+        <div className="z-[5] absolute w-full h-full bg-gradient-to-b from-[0%] from-[#2a0335]/50 via-[43%] via-[#6258D1]/50 to-[100%] to-[#00CBC1]/50 blue-light-top"></div>
+        <div className="rg-regis-detail-purple-light-middle absolute bottom-[-5rem] bg-[#97156A] w-[1100px] h-[900px] rounded-full blur-[100px] z-[0]"></div>
         <Image
-          className="rg-regis-gradient-light-bg absolute z-[1] w-full bottom-[40%] sm:bottom-[40%] lg:bottom-[-30%] h-[35rem] sm:h-auto"
-          src={"/backgrounds/GradientLightBG.webp"}
-          width={1000}
-          height={1000}
-          alt="background-gradient"
-        />
-        <Image
-          className="rg-regis-light-middle  absolute z-[2] w-full bottom-[40%] sm:bottom-[35%] lg:bottom-[20%] h-auto"
-          src={"/backgrounds/LightTengah.webp"}
-          width={1000}
-          height={1000}
-          alt="light-tengah"
-        />
-        <Image
-          className="rg-regis-blue-building absolute z-[3] bottom-[40%] sm:bottom-[35%] lg:bottom-[-4%] w-screen h-auto"
           src={"/backgrounds/BangunanBelakangBiru.svg"}
+          alt="Background"
           width={100}
           height={100}
-          alt="bagunan-biru"
+          className="z-[1] rg-regis-blue-building absolute w-screen h-auto bottom-0"
         />
         <Image
-          className="rg-regis-purple-building absolute z-[4] bottom-[35%] sm:bottom-[30%] lg:bottom-[-4%] w-screen h-auto"
           src={"/backgrounds/BangunanDepanUngu.svg"}
+          alt="Background"
           width={100}
           height={100}
-          alt="bangunan-ungu"
+          className="z-[2] rg-regis-purple-building absolute w-screen h-auto bottom-0"
         />
         <Image
-          className="rg-regis-front-light-gradient absolute z-[5] bottom-[30%] sm:bottom-[25%] lg:bottom-0 w-screen h-auto"
-          src={"/backgrounds/FrontGradientLightBG.svg"}
+          src={"/backgrounds/Stairs.svg"}
+          alt="Stairs"
           width={100}
           height={100}
-          alt="front-light"
+          className="z-[10] rg-regis-stairs absolute w-screen h-auto bottom-[-1rem]"
         />
-        <div className="rg-regis-form-container relative z-20 flex gap-4 flex-col items-center justify-center min-h-screen w-full px-4">
+
+        <div className="relative z-20 flex flex-col items-center justify-center min-h-screen w-full px-4">
           <Image
             className="rg-competition-text z-[10] w-[60%] sm:w-1/3 h-auto"
             src={"/register/AccountTitle.svg"}
@@ -64,14 +51,6 @@ export default async function Page() {
           />
           <SignUpForm />
         </div>
-        <div className="rg-under-stair bg-[#090A1E] absolute w-screen h-[20rem] bottom-[-10%] z-[9]"></div>
-        <Image
-          className="rg-regis-stairs absolute z-[10] w-full bottom-[28%] sm:bottom-[20%] lg:bottom-[-8%] h-auto"
-          src={"/backgrounds/Stairs.svg"}
-          width={1000}
-          height={1000}
-          alt="background-gradient"
-        />
       </div>
     </div>
   );
